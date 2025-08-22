22a64afa002b9bd9bed48ff0da97a69d1182bc82
Sergio <sergio@dual-link.com>
2025-08-22 11:29:13 +0200

    axios error

--- hooks/useGetVideogames.ts
+++ hooks/useGetVideogames.ts
@@ -11,7 +11,9 @@ export function useGetVideogames() {
     const fetchVideogames = async () => {
       try {
         const host = process.env.EXPO_PUBLIC_HOST;
-
+        if (!host) {
+          throw new Error("Host not found. Please set EXPO_PUBLIC_HOST in your environment variables.");
+        }
         const response = await axios.post<Videogame[]>(
           `http://${host}/api/videogames/`,
           {
