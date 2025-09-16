40d56dea7d21d73f8221a7ad501d0d376d1ef8ac
Alastair7 <alexandru.ban.drmer@gmail.com>
2025-09-16 20:49:00 +0200

    Add supabase context provider

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,10 +1,43 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { supabase } from "@/lib/supabase";
+import React, { useState } from "react";
+import { View, Text, Alert, Button, TextInput } from "react-native";
 
-export const LoginScreen = () => {
+export default function LoginScreen() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleLogin = async () => {
+    setLoading(true);
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    if (error) {
+      Alert.alert(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <View>
       <Text>GGTime</Text>
+      <TextInput
+        onChangeText={setEmail}
+        value={email}
+        keyboardType="email-address"
+      />
+      <TextInput
+        onChangeText={setPassword}
+        value={password}
+        keyboardType="visible-password"
+      />
+
+      <View>
+        <Button title="Sign In" disabled={loading} onPress={handleLogin} />
+      </View>
     </View>
   );
-};
+}
