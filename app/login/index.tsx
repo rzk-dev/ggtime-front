e3bd2d048fff4812dc91a20bda500587c54b9c74
Sergio <sergio@dual-link.com>
2025-09-22 12:43:45 +0200

    apple auth?

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,84 +1,61 @@
-import { supabase } from "@/lib/supabase";
-import React, { useState } from "react";
-import { View, Text, Alert, Button, TextInput, StyleSheet } from "react-native";
+import React from 'react';
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import type { StackNavigationProp } from '@react-navigation/stack';
 import type { RootStackParamList } from '../routes/routes';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import * as AppleAuthentication from 'expo-apple-authentication';
+
+
 
 type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Login'>;
 
 export default function LoginScreen() {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
   const navigation = useNavigation<LoginScreenNavigationProp>();
 
+  const handleLoginWithGoogle = async () => {
+    // Aquí validas el login y luego navegas
+    navigation.navigate('Home');
+  };
 
-  const handleLogin = async () => {
-    setLoading(true);
-
-    const { error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password,
-    });
-
-    //navigation.navigate('Home');
-
-    if (error) {
-      Alert.alert(error.message);
-      setLoading(false);
+  const handleLoginWithApple = async () => {
+    try {
+      const credential = await AppleAuthentication.signInAsync({
+        requestedScopes: [
+          AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+          AppleAuthentication.AppleAuthenticationScope.EMAIL,
+        ],
+      });
+      // signed in
+      console.log(credential);
+      navigation.navigate('Home');
+    } catch (e) {
+      if (typeof e === 'object' && e !== null && 'code' in e && (e as { code?: string }).code === 'ERR_REQUEST_CANCELED') {
+        // handle that the user canceled the sign-in flow
+      }
     }
   };
 
   return (
     <View style={styles.container}>
-      <Text>GGTime</Text>
-      <TextInput style={styles.textfield}
-        onChangeText={setEmail}
-        value={email}
-        keyboardType="email-address"
-        placeholder="email"
-      />
-      <TextInput style={styles.textfield}
-        onChangeText={setPassword}
-        value={password}
-        keyboardType="visible-password"
-        placeholder="password"
-      />
-
-      <View>
-        <Button title="Sign In" disabled={loading} onPress={handleLogin} />
-      </View>
-
-      {/*<View style={{ marginBottom: 20 }}>
-        <FontAwesome.Button name="google" backgroundColor="#aa2929ff" onPress={handleLogin}>
-          Sign in with Google
-        </FontAwesome.Button>
-      </View>
-
-      <View>
-        <FontAwesome.Button name="apple" backgroundColor="#202020ff" onPress={handleLogin}>
-          Sign in with Apple
-        </FontAwesome.Button>
-      </View>*/}
+        <View style={{ marginBottom: 20 }}>
+            <FontAwesome.Button name="google" backgroundColor="#aa2929ff" onPress={handleLoginWithGoogle}>
+            Sign in with Google
+            </FontAwesome.Button>
+        </View>
+        <View>
+            <FontAwesome.Button name="apple" backgroundColor="#202020ff" onPress={handleLoginWithApple}>
+            Sign in with Apple
+            </FontAwesome.Button>
+        </View>
 
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  textfield: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-    width: 200,
-    paddingHorizontal: 10,
   },
 });
--- app/login/loginScreen.tsx
+++ app/login/loginScreen.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import type { StackNavigationProp } from '@react-navigation/stack';
 import type { RootStackParamList } from '../routes/routes';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import * as AppleAuthentication from 'expo-apple-authentication';
+
 
 
 type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Login'>;
@@ -11,20 +13,41 @@ type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Login'
 export default function LoginScreenTEST() {
   const navigation = useNavigation<LoginScreenNavigationProp>();
 
-  const handleLogin = async () => {
+  const handleLoginWithGoogle = async () => {
     // Aquí validas el login y luego navegas
+    navigation.navigate('Home');
+  };
+
+  const handleLoginWithApple = async () => {
+    try {
+      const credential = await AppleAuthentication.signInAsync({
+        requestedScopes: [
+          AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+          AppleAuthentication.AppleAuthenticationScope.EMAIL,
+        ],
+      });
+      // signed in
+      console.log(credential);
+      // sample response provided below
+    } catch (e) {
+      if (typeof e === 'object' && e !== null && 'code' in e && (e as { code?: string }).code === 'ERR_REQUEST_CANCELED') {
+        // handle that the user canceled the sign-in flow
+      } else {
+        // handle other errors
+      }
+    }
     navigation.navigate('Home');
   };
 
   return (
     <View style={styles.container}>
         <View style={{ marginBottom: 20 }}>
-            <FontAwesome.Button name="google" backgroundColor="#aa2929ff" onPress={handleLogin}>
+            <FontAwesome.Button name="google" backgroundColor="#aa2929ff" onPress={handleLoginWithGoogle}>
             Sign in with Google
             </FontAwesome.Button>
         </View>
         <View>
-            <FontAwesome.Button name="apple" backgroundColor="#202020ff" onPress={handleLogin}>
+            <FontAwesome.Button name="apple" backgroundColor="#202020ff" onPress={handleLoginWithApple}>
             Sign in with Apple
             </FontAwesome.Button>
         </View>
